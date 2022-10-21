4bdd95120ecec8f2f40020c35a975dcd8f5745ec
CMarLar <c.marina.larrosa@gmail.com>
2022-10-21 13:01:38 +0200

    Clase Vector

--- POO/week_2/Typescript/vector.ts
+++ POO/week_2/Typescript/vector.ts
@@ -1,11 +1,50 @@
-class Vector
+export class Vector
 {
     private elements: number[]
 
-        constructor(n: number, k: number)
-        {
-            for (let i = 0; i < n; i++) {
-                this.elements.push(Math.floor(Math.random()*k));
+    constructor(n: number, k: number)
+    {
+        this.elements = [];
+        for (let i = 0; i < n; i++) {
+            this.elements.push(Math.floor(Math.random()*k));
+        }
+    }
+
+    public print():void{
+        console.log(this.elements);
+        
+    }
+
+    public add(v1:Vector):Vector{
+        
+        for (let i = 0; i < v1.elements.length; i++) {
+            v1.elements[i] = this.elements[i] + v1.elements[i]
+        }
+        return v1;
+    }
+
+    public subs(v1:Vector):Vector{
+        for (let i = 0; i < v1.elements.length; i++) {
+            v1.elements[i] = this.elements[i] - v1.elements[i]
+        }
+        return v1;
+    }
+
+    public mult(v1:Vector):Vector{
+        for (let i = 0; i < v1.elements.length; i++) {
+            v1.elements[i] = this.elements[i] * v1.elements[i]
+        }
+        return v1;
+    }
+
+    public multNumber(n:number):Vector{
+            let vector1: Vector = new Vector(this.elements.length,10);//creamos nuevo vector usando el constructor con la misma length que this.elements y nums aleatorios entre 1 y 10
+            
+            for (let i = 0; i < this.elements.length; i++) {
+            vector1.elements[i] = this.elements[i] * n
             }
+            return vector1;
         }
 }
+
+
