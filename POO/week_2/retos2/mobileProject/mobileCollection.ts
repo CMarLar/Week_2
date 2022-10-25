db510d8cf4ec5425bf4561f3b0bb55343d599461
CMarLar <c.marina.larrosa@gmail.com>
2022-10-25 16:29:10 +0200

    correcciones en mobileCollection

--- POO/week_2/retos2/mobileProject/mobileCollection.ts
+++ POO/week_2/retos2/mobileProject/mobileCollection.ts
@@ -26,13 +26,7 @@ export class MobileCollection
 
     public printCollection(): void{
         for (let i = 0; i < this.mobiles.length; i++) {
-            console.log("The characteristics of the mobile " + this.mobiles[i].getName() + "are:" + "\n" + 
-                        "Name: " + this.mobiles[i].getName() + "\n" +
-                        "Trademark: " + this.mobiles[i].getTrademark() + "\n" +
-                        "Model: " + this.mobiles[i].getModel() + "\n" +
-                        "Color: " + this.mobiles[i].getColor() + "\n" +
-                        "Price: " + this.mobiles[i].getPrice() + "\n"
-                        );
+            console.log(this.mobiles[i].printInfo());
             
         }
 
